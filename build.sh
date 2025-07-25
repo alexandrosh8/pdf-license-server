7a35faeb3e86a8c8b4a469b6ddb05b42dc5eae68
alexandrosh8 <halexandros25@gmail.com>
2025-07-25 15:14:39 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
-#!/usr/bin/env bash
-# Exit on error
+#!/bin/bash
 set -o errexit
+
+echo "Starting build process..."
 
 # Update pip to latest version
 python -m pip install --upgrade pip
 
 # Install dependencies
+echo "Installing Python dependencies..."
 pip install -r requirements.txt
 
 # Create releases directory if it doesn't exist
